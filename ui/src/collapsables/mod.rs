aecbaad567178ee55b9a09317d7f080a376dab29
writatas <jnelson.writing@gmail.com>
2023-08-07 19:25:39 +0200

    added basic window functionality where windows will appear depending on the cursor position.

--- ui/src/collapsables/mod.rs
+++ ui/src/collapsables/mod.rs
@@ -1,2 +1,35 @@
-
+use eframe::egui::{Vec2, Ui};
 //TODO new ttrpg_entity 
+// returns the ui height and width as a egui::Vec2 in order to calculate ui sizes
+pub fn configuration_ui(ui: &mut Ui) -> Vec2 {
+    let config_ui = ui.group(|ui| {
+        ui.horizontal_wrapped(|ui| {
+            ui.strong("configuration window test");
+        });
+    });
+    config_ui.response.rect.size()
+}
+pub fn selected_ttrpg_elements(ui: &mut Ui) -> Vec2 {
+    let selected_ttrpg_ui = ui.group(|ui| {
+        ui.horizontal_wrapped(|ui| {
+            ui.strong("selected_ttrpg window test");
+        });
+    });
+    selected_ttrpg_ui.response.rect.size()
+}
+pub fn dice_rolls_and_creation_history (ui: &mut Ui) -> Vec2 {
+    let dice_rolls_and_creation_history_ui = ui.group(|ui| {
+        ui.horizontal_wrapped(|ui| {
+            ui.strong("dice_rolls_and_creation_history_ui test");
+        });
+    });
+    dice_rolls_and_creation_history_ui.response.rect.size()
+}
+pub fn saved_configs_window(ui: &mut Ui) -> Vec2 {
+    let saved_configs_window_ui = ui.group(|ui| {
+        ui.horizontal_wrapped(|ui| {
+            ui.strong("saved_configs_window ui test");
+        });
+    });
+    saved_configs_window_ui.response.rect.size()
+}
--- ui/src/desktop/mod.rs
+++ ui/src/desktop/mod.rs
@@ -4,6 +4,7 @@ use std::sync::Arc;
 use eframe::egui;
 use egui::{Pos2};
 use gm_helper_corelibrary::{TtrpgEntity, Story, Attribute, Counter, Skill, Table};
+use crate::collapsables::*;
 
 pub struct MainWindow {
     selected_database:Cell<String>,
@@ -40,38 +41,56 @@ impl eframe::App for MainWindow {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         // Track the cursor position to expand a detract the sections of the main window
         let cursor_pos = track_cursor_position(ctx);
-        let upper_x = ctx.used_size().to_pos2().x;
-        let upper_y = ctx.used_size().to_pos2().y;
+        let upper_x = ctx.available_rect().size().x;
+        let upper_y = ctx.available_rect().size().y;
         // Cursor tracking is in this order; Left, Right, Top, Bottom
         if cursor_pos.x < 5.0 && cursor_pos.x > 0.0 && self.selected_ttrpg_elements.get() == false {
             self.selected_ttrpg_elements.set(true);
         }
-        else {
-            self.selected_ttrpg_elements.set(false);
-        }
         if cursor_pos.x > (upper_x - 5.0) && cursor_pos.x < upper_x && self.saved_configs_window.get() == false {
             self.saved_configs_window.set(true);
-        }
-        else {
-            self.saved_configs_window.set(false);
         }
         if cursor_pos.y < 5.0 && cursor_pos.y > 0.0 && self.configure_creation_window.get() == false {
             self.configure_creation_window.set(true);
         }
-        else {
-            self.configure_creation_window.set(false);
-        }
         if cursor_pos.y > (upper_y - 5.0) && cursor_pos.y < upper_y && self.dice_rolls_creation_history.get() == false {
             self.dice_rolls_creation_history.set(true);
         }
-        else {
-            self.dice_rolls_creation_history.set(false);
-        }
 
-        // CONFIGURATION WINDOW
+        // SELECTED TTRPG WINDOW - left
+        if self.selected_ttrpg_elements.get() {
+            egui::SidePanel::left("selected_ttrpgs_window").show(ctx, |ui| {
+                let selected_ttrpg_window_size = selected_ttrpg_elements(ui);
+                if cursor_pos.x > selected_ttrpg_window_size.x {
+                    self.selected_ttrpg_elements.set(false);
+                }
+            });
+        }
+        // SAVED CONFIGS WINDOW- right
+        if self.saved_configs_window.get() {
+            egui::SidePanel::right("saved_configs_window").show(ctx, |ui| {
+                let saved_configs_window_size = saved_configs_window(ui);
+                if cursor_pos.x < (upper_x - saved_configs_window_size.x) {
+                    self.saved_configs_window.set(false);
+                }
+            });
+        }
+        // CONFIGURATION WINDOW -top
         if self.configure_creation_window.get() {
             egui::TopBottomPanel::top("configure_creation_window").show(ctx, |ui| {
-
+                let config_window_size = configuration_ui(ui);
+                if cursor_pos.y > config_window_size.y {
+                    self.configure_creation_window.set(false);
+                }
+            });
+        }
+         // DICE ROLLS CREATION HISTORY - botton
+         if self.dice_rolls_creation_history.get() {
+            egui::TopBottomPanel::bottom("dice_rolls_and_creation_history_window").show(ctx, |ui| {
+                let dice_rolls_and_creation_history_window_size = dice_rolls_and_creation_history(ui);
+                if cursor_pos.y < (upper_y - dice_rolls_and_creation_history_window_size.y) {
+                    self.dice_rolls_creation_history.set(false);
+                }
             });
         }
     }
