afe7c6be77b34922e49720aca18ea17e1af997d9
writatas <jnelson.writing@gmail.com>
2023-08-29 19:41:07 +0200

    Can now add and delete entire ttrpgs (database storage not implemented)

--- ui/src/collapsables/mod.rs
+++ ui/src/collapsables/mod.rs
@@ -9,29 +9,31 @@ use rand::{distributions::Alphanumeric, Rng};
 pub fn configuration_ui(ui: &mut Ui, ttrpgs: &mut Vec<TtrpgEntity>, new_database: &mut Cell<String>, new_ttrpg: &mut Cell<TtrpgEntity>) -> Vec2 { // Select database and load elements
     let config_ui = ui.group(|ui| {
             ui.group(|ui|{
-                if ui.button("Create database").clicked() {
-                    let dummy_ttrpg = TtrpgEntity::new(false, None, "dummy".to_string(), Some(new_database.get_mut()));
-                    let (db_string, string_len) = (new_database.get_mut().clone(), new_database.get_mut().clone().len());
-                    // Create the database as long under condition checks:
-                    // cannot contain whitespace, must be alphabetic,
-                    // and between the lengths of 0 to 50
-                    if !db_string.contains(char::is_whitespace) &&
-                    db_string.contains(char::is_alphabetic) && 
-                    string_len > 0 &&
-                    string_len < 50 &&
-                    !dummy_ttrpg.database.is_file() {
-                        let connection = Connection::open(dummy_ttrpg.database.as_os_str()).expect("Database creation failed!");
-                        let query = "
-                            CREATE table ttrpgs (
-                                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                                date DATETIME DEFAULT CURRENT_TIMESTAMP,
-                                json_string TEXT NOT NULL
-                            );
-                        ";
-                        connection.execute(query).expect("SQL query failure; could not create database...");
+                ui.horizontal(|ui| {
+                    if ui.button("Create database").clicked() {
+                        let dummy_ttrpg = TtrpgEntity::new(false, None, "dummy".to_string(), Some(new_database.get_mut()));
+                        let (db_string, string_len) = (new_database.get_mut().clone(), new_database.get_mut().clone().len());
+                        // Create the database as long under condition checks:
+                        // cannot contain whitespace, must be alphabetic,
+                        // and between the lengths of 0 to 50
+                        if !db_string.contains(char::is_whitespace) &&
+                        db_string.contains(char::is_alphabetic) && 
+                        string_len > 0 &&
+                        string_len < 50 &&
+                        !dummy_ttrpg.database.is_file() {
+                            let connection = Connection::open(dummy_ttrpg.database.as_os_str()).expect("Database creation failed!");
+                            let query = "
+                                CREATE table ttrpgs (
+                                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                    date DATETIME DEFAULT CURRENT_TIMESTAMP,
+                                    json_string TEXT NOT NULL
+                                );
+                            ";
+                            connection.execute(query).expect("SQL query failure; could not create database...");
+                        }
                     }
-                }
-                ui.text_edit_singleline(new_database.get_mut())
+                    ui.text_edit_singleline(new_database.get_mut())
+                });
             });
         
         new_ttrpg.get_mut().active.set(true);
@@ -68,41 +70,39 @@ pub fn selected_ttrpg_elements(ui: &mut Ui, ttrpgs: &mut Vec<TtrpgEntity>) -> Ve
             ttrpg_without_databases += 1;
         }
     }
-    let existing_paths = std::fs::read_dir("./saved_dbs/").unwrap();
+    let existing_paths = std::fs::read_dir("./saved_dbs/").expect("Could not read existing database paths");
     let mut paths = Vec::new();
     for p in existing_paths {
         paths.push(p.unwrap().path().display().to_string());
     }
     let mut dbs_to_delete: Vec<String> = Vec::new();
+    let mut ttrpgs_to_delete:Vec<(usize, String)> = Vec::new();
     let selected_ttrpg_ui = ui.group(|ui| {
         ui.strong(format!("Number of ttrpg entities: {}", ttrpgs.len()));
         ui.strong(format!("Number of ttrpg entities without chosen databases: {}", ttrpg_without_databases));
         ScrollArea::vertical().show(ui, |ui| {
-            for ttrpg in ttrpgs.iter_mut() {
+            for (index, ttrpg) in ttrpgs.iter_mut().enumerate() {
                 ui.group(|ui| {
                     ui.strong(ttrpg.name.clone());
                     ui.horizontal_wrapped(|ui| {
                         let active_text = if ttrpg.active.get() {"Active"} else {"Not Active"};
                         ui.checkbox(ttrpg.active.get_mut(), active_text);
+                        if ui.small_button("Delete").clicked() {
+                            ttrpgs_to_delete.push((index, ttrpg.name.clone()))
+                        }
                     });
                     ui.label(format!("Number of elements {}", ttrpg.elements.len()));
                     // Database selection and creation per ttrpg element
                     ui.horizontal(|ui| {
                         // Select the Database this ttrpg should save to
-                        if ttrpg.database.as_os_str().is_empty() {
-                            ui.label("No database to save to!");
-                        } else {
-                            ui.label("Selected database: ");
-                        }
-
+                        ui.label("Selected database: ");
                         ComboBox::from_id_source(format!("{}{}", &ttrpg.name, ttrpg.id))
                         .selected_text(ttrpg.database.as_os_str().to_str().expect("Could not retrieve selected text"))
                         .show_ui(ui, |ui| {
-                            let no_dbs_found = "No databases found. Create a new one!".to_string();
                                 for path in paths.iter() {
-                                        let mut current_path = Some(ttrpg.database.as_os_str().to_str().expect("could not get selectable value").to_string());
-                                        let path = if path.len() > 12 {path} else {&no_dbs_found};
-                                        let selectable_value = ui.selectable_value(&mut current_path, Some(path.to_string().clone()), path.clone());
+                                        let mut current_path = "".to_string();
+                                        let path_cut = &path.clone()[12..];
+                                        let selectable_value = ui.selectable_value(&mut current_path, path_cut.to_string() ,path_cut.to_string());
                                         if selectable_value.clicked() {
                                             ttrpg.database = Path::new(&path).to_path_buf();
                                         }
@@ -116,8 +116,16 @@ pub fn selected_ttrpg_elements(ui: &mut Ui, ttrpgs: &mut Vec<TtrpgEntity>) -> Ve
                     });
                 });
             }
+            
         });
     });
+    if ttrpgs_to_delete.len() > 0 {
+        for delete in ttrpgs_to_delete.iter() {
+            ttrpgs.remove(delete.0);
+            println!("Deleted ttrpg: {}", delete.1);
+        }
+        ttrpgs_to_delete.clear();
+    }
     if dbs_to_delete.len() > 0 {
         let dummy_id: Option<u32> = None;
         let dummy_db: Option<&str> = None;
